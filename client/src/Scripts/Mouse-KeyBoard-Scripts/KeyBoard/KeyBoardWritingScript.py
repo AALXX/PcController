138dd22878d7947bc32a34b1bfa7a48a9f92ef4b
Alex-Maxx <alexn.serban@gmail.com>
2021-08-12 00:36:11 +0300

    Added writing request functionality

--- client/src/Scripts/Mouse-KeyBoard-Scripts/KeyBoard/KeyBoardWritingScript.py
+++ client/src/Scripts/Mouse-KeyBoard-Scripts/KeyBoard/KeyBoardWritingScript.py
@@ -4,10 +4,8 @@ import time
 
 del sys.argv[0]
 
-
 if(len(sys.argv) == 2 and sys.argv[1] != ""):
     for i in range(0 ,int(sys.argv[1])):
         pyautogui.typewrite(sys.argv[0] + " \n", interval=0.1)
 else:        
     pyautogui.typewrite(sys.argv[0], interval=0.1)
-
